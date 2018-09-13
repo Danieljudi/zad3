b2e9aa11cae072774a340191757a406ee7f7ea21
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-13 12:44:24 +0200

    for

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int tab[]=new int[5];
+        tab[0]=6;
+        tab[1]=9;
+        tab[2]=3;
+        tab[3]=8;
+        tab[4]=2;
+        for (int i=0;i<5;i++)
+        System.out.println("element tablicy = "+ tab[i]);
+    }
+}
